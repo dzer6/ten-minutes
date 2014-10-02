d3eaa7926ebae067b0763bc4aa77bd464ffd2687
Andrew Panfilov <andrei.panfilov@gmail.com>
2014-10-02 20:40:35 +0300

    change license

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject ten-minutes "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "Apache License, version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
 
   :dependencies [[org.clojure/clojure         "1.6.0"]
                  [compojure                   "1.1.6"]
